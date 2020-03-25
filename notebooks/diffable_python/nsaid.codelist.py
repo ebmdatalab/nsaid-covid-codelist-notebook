a5cb04991ea0f750c47b412fd3b4f5555a61dcec
Brian <brianmkcebm@gmail.com>
2020-03-25 12:02:22 +0000

    first commit NSAID codelist

---
+++ notebooks/diffable_python/nsaid.codelist.py
@@ -0,0 +1,42 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+from ebmdatalab import bq
+import os
+
+# +
+sql = '''WITH bnf_codes AS (
+  SELECT bnf_code FROM hscic.presentation WHERE 
+  bnf_code LIKE '1001010%' #bnf section non-steroidal anti-inflammatory drugs
+  )
+
+SELECT "vmp" AS type, id, bnf_code, nm
+FROM dmd.vmp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+UNION ALL
+
+SELECT "amp" AS type, id, bnf_code, descr
+FROM dmd.amp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+ORDER BY type, bnf_code, id'''
+
+nsaid_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','nsaid_codelist.csv'))
+nsaid_codelist.head(10)
+# -
+
+
